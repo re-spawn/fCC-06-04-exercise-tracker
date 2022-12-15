60c461cb90a4b50b9eea5e0fea990bd21afe8e36
re-spawn <rob@robanthony.ca>
2022-12-15 15:15:47 -0330

    feat: post-users

--- index.js
+++ index.js
@@ -10,7 +10,48 @@ app.get('/', (req, res) => {
 });
 
 
+const bodyParser = require('body-parser');
+app.use('/', bodyParser.urlencoded({extended: false}));
 
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env['MONGO_URI'], {useNewUrlParser: true, useUnifiedTopology: true});
+
+let userSchema = new mongoose.Schema({
+  username: {type: String, required: true}
+});
+let User = mongoose.model('User', userSchema);
+
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  if (username == "") {
+    console.log("empty username")
+    return;
+  }
+  User.exists({username: username}, function(err, user) {
+    if (err) {
+      console.log("failed User.exists");
+      console.error(err);
+      return;
+    } else if (user != null) {
+      console.log("user already exists");
+      return;
+    } else {
+      User.create({username: username}, function(err, user) {
+        if (err) {
+          console.log("failed User.create");
+          console.error(err);
+          return;
+        } else {
+          res.json({
+            username: user.username,
+            _id: user._id
+          });
+        }
+      })
+    }
+  });
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
